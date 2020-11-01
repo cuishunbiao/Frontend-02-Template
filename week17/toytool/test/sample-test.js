dfca646e124e0d2eeee65eaf494be6ccc48f6bca
cuishunbiao <860026526@qq.com>
2020-11-01 16:00:24 +0800

    生成 toytool

---
+++ week17/toytool/test/sample-test.js
@@ -0,0 +1,8 @@
+var assert = require('assert');
+import { format, parse } from 'path';
+
+describe('1+1=2', function () {
+    it('1+1===2', function () {
+        assert.equal(1 + 1, 2);
+    })
+})
